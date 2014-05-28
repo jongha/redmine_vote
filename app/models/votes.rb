aea22b0fd2e31b799eee2e0a7e6318befb237618
Lothar Gesslein <gesslein@b1-systems.de>
2014-05-28 22:59:13 +0200

    Allow users to change their own vote

--- app/models/votes.rb
+++ app/models/votes.rb
@@ -3,7 +3,10 @@ class Votes < ActiveRecord::Base
 
   def add_vote(message_id, user_id, point = 0)
     votes = Votes.find(:first, :conditions => ['message_id = ? and user_id = ?', message_id, user_id])
-    unless votes
+    if votes
+      votes.point = point.nil? ? 0 : point
+      votes.save!
+    else
       votes = Votes.new
       votes.message_id = message_id
       votes.user_id = user_id
