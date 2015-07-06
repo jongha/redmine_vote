a0d1165d11a449c57d5369e5e9af610b09068fa1
Jethro Yu <jethro.yu@hopebaytech.com>
2015-07-07 01:19:57 +0800

    Undo vote by clicking same button ... as same as stackoverflow

--- app/models/votes.rb
+++ app/models/votes.rb
@@ -1,17 +1,18 @@
 class Votes < ActiveRecord::Base
   unloadable
 
-  def add_vote(message_id, user_id, point = 0)
-    votes = Votes.where('message_id = %d and user_id = %d' % [message_id, user_id]).limit(1)
+  def add_vote(message_id, user_id, point = "0")
+    votes = Votes.where('message_id = %d and user_id = %d' % [message_id, user_id]).first
 
-    unless votes.count == 0
-      votes.point = point.nil? ? 0 : point
+    point = point.to_i
+    if votes
+      votes.point = (votes.point == point)? 0 : point
       votes.save!
     else
       votes = Votes.new
       votes.message_id = message_id
       votes.user_id = user_id
-      votes.point = point.nil? ? 0 : point
+      votes.point = point
       votes.save!
     end
 
@@ -27,7 +28,7 @@ class Votes < ActiveRecord::Base
       "plus" => Votes.where('message_id = %d and point > 0' % message_id).sum(:point),
       "minus" => Votes.where('message_id = %d and point < 0' % message_id).sum(:point),
       "zero" => Votes.where('message_id = %d and point = 0' % message_id).sum(:point),
-      "vote" => Votes.where('message_id = %d and user_id = %d' % [message_id, user_id]).count(:point),
+      "vote" => Votes.where('message_id = %d and user_id = %d' % [message_id, user_id]).sum(:point),
     }
   end
 end
